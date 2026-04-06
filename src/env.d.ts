c2473170e9160bd588dc712929ab612a86c1f33c
Eric Mikkelsen <eric@erics-air.mynetworksettings.com>
2026-04-06 12:58:01 -0500

    chore(color-layers): add wasm image optimization dependencies

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,2 +1,7 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
+
+declare module "oxipng-wasm/oxipng_wasm_bg.js" {
+	export const __wbg_set_wasm: (wasm: unknown) => void;
+	export const encode: (pngBytes: Uint8Array, level?: number) => Uint8Array;
+}
